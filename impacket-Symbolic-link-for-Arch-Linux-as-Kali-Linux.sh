f777f0c613f40a561e707be170940ce71928a199
NoobGajen <sudogajendra@gmail.com>
2024-05-03 13:05:34 +0545

    Sample SS added

--- impacket-Symbolic-link-for-Arch-Linux-as-Kali-Linux.sh
+++ impacket-Symbolic-link-for-Arch-Linux-as-Kali-Linux.sh
@@ -5,7 +5,7 @@ for i in $(pacman -Q | grep 'impacket\|python-impacket-git'| awk '{print $1}');
 do echo -e "$i package installed found... \n"
     
     #### executing for loop to store impacket binary in j variable.
-    for j in $(pacman -Ql $i | grep -i '/usr/bin/[A-z0-9]' | awk '{print $2}');
+    for j in $(pacman -Ql $i | grep -i '/usr/bin/[A-z0-9._-]\+' | awk '{print $2}');
     do
         #### Creating symbolic links as /usr/bin/impacket-xxxxxxxx
         sudo ln -sfv $(echo "$j"|tr '\n' ' ') $(echo "/usr/bin/impacket-$(basename $j .py)");
