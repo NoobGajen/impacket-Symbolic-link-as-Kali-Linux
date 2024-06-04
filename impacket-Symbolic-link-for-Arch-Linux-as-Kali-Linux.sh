11229d07836a0f624cd010a5ec11834587b1df51
NoobGajen <sudogajendra@gmail.com>
2024-06-04 15:18:50 +0545

    REgex improved and Script optimized

--- impacket-Symbolic-link-for-Arch-Linux-as-Kali-Linux.sh
+++ impacket-Symbolic-link-for-Arch-Linux-as-Kali-Linux.sh
@@ -1,20 +1,17 @@
 #!/bin/bash
 
-# Check if impacket is installed
-if pacman -Q | grep -q 'impacket\|python-impacket-git'; then
-    
-    # Running for loop to get impacket package name which is installed in your arch linux system.
-    for i in $(pacman -Q | grep 'impacket\|python-impacket-git'| awk '{print $1}');
-    do
-        echo -e "\033[32m$i\033[0m package found installed in your system:)"
-        echo -e "Now, creating symbolic link for impacket binaries...\n"
-        
-        # Executing for loop to store impacket binary in j variable.
-        for j in $(pacman -Ql $i | grep -i '/usr/bin/[A-z0-9._-]\+' | awk '{print $2}');
-        do
-            #### Creating symbolic links as /usr/bin/impacket-xxxxxxxx
-            sudo ln -sfv $(echo "$j"|tr '\n' ' ') $(echo "/usr/bin/impacket-$(basename $j .py)");
-        done;
+# Check if impacket or python-impacket-git is installed and get the package name
+package=$(pacman -Qs | awk '{print $1}' | grep -E '^local/impacket$|^local/python-impacket-git$')
+
+# Check if the package variable is non-empty (i.e., a matching package is found)
+if [ -n "$package" ]; then
+    echo -e "\033[32m$(basename $package local/)\033[0m package found installed in your system:)"
+    echo -e "Now, creating symbolic link for impacket binaries...\n"
+
+    # Get the list of binaries for the package and create symbolic links
+    binaries=$(pacman -Ql $package | grep -i '/usr/bin/[A-Za-z0-9._-]\+' | awk '{print $2}')
+    for binary in $binaries; do
+        sudo ln -sfv "$binary" "/usr/bin/impacket-$(basename "$binary" .py)"
     done
 else
     echo -e "Impacket is not installed in your Arch Linux."
